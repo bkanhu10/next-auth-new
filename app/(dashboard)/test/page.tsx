e08028bc0b90b1e763886bf84a8a10211351450d
bkanhu <bkanhu@skentino.com>
2025-09-08 11:37:59 +0530

    Refactor Navbar component to include session logging and redirect to login if not authenticated

--- app/(dashboard)/test/page.tsx
+++ app/(dashboard)/test/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
+  const router = useRouter();
+  console.log("Session data:", session);
+  console.log("Session status:", status);
 
+  if(!session) router.push('/login?callbackUrl=/test');
   if (status === "loading") return <p>Loading...</p>;
-
   return (
     <nav>
       Client-side Dashboard Page
